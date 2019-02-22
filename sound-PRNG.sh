4faa938ea27d6989fcc511766a9ea817d4bd8c0a
smoffgk <techinfo@fastmail.fm>
2019-02-22 09:24:07 +0000

    Updated splicing, so now splits the initial hash into 4 before re-hashing

--- sound-PRNG.sh
+++ sound-PRNG.sh
@@ -6,7 +6,7 @@
 COUNT=0 
 TOTAL=5
 SAMPLE_DURATION_SECONDS=2
-
+OUTPUT=my.random
 clear
 echo "SoundRandom sampler started..."
 echo "------------------------------"
@@ -15,13 +15,16 @@ while [ $COUNT -lt $TOTAL ]; do
 	
 	#Captures 5 secs @ 48,000 sample rate
 	arecord -q -vv -fdat -d$SAMPLE_DURATION_SECONDS sample.wav
-	#Creates a double sha512, taking the middle third only to increase uniqueness, stripping out only digits	
-	sha512sum sample.wav | cut -c32-97 | sha512sum | cut -c32-97 | grep [0-9] -o | awk '{print}' ORS='' >> myRandom
+	#SHA512 hashes the sample file, splits into 4 pieces and uses that to generate a new SHA512 hash that is stored in the output file
+	sha512sum sample.wav | cut -c1-32 | sha512sum | grep [0-9] -o | awk '{print}' ORS='' >> $OUTPUT
+	sha512sum sample.wav | cut -c33-64 | sha512sum | grep [0-9] -o | awk '{print}' ORS='' >> $OUTPUT
+	sha512sum sample.wav | cut -c65-96 | sha512sum | grep [0-9] -o | awk '{print}' ORS='' >> $OUTPUT
+	sha512sum sample.wav | cut -c97-128 | sha512sum | grep [0-9] -o | awk '{print}' ORS='' >> $OUTPUT
 	rm -rf sample.wav
 	COUNT=$(( $COUNT + 1 ))
 
 done
 
 echo ""
-echo "SoundRandom sampler completed.  $TOTAL x $SAMPLE_DURATION_SECONDS second samples created, encoded and stored."
-echo "----------------------------------------------------------------------------------"
+echo "SoundRandom sampler completed.  $TOTAL x $SAMPLE_DURATION_SECONDS second samples created, encoded and stored in $OUTPUT."
+echo "-----------------------------------------------------------------------------------------------------------------------------"
